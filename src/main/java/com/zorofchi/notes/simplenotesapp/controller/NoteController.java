5543fd7ddd93624cfdb63ff96a07e1119c7d02a4
esmaeilsadeghijob <esmaeilsadeghijob@gmail.com>
2025-09-18 10:40:43 +0330

    added noteController file

--- src/main/java/com/zorofchi/notes/simplenotesapp/controller/NoteController.java
+++ src/main/java/com/zorofchi/notes/simplenotesapp/controller/NoteController.java
@@ -1,4 +1,19 @@
 package com.zorofchi.notes.simplenotesapp.controller;
 
+
+import com.zorofchi.notes.simplenotesapp.model.Note;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/note"})
 public class NoteController {
+
+    public Note save(Note noteRequest){
+
+
+
+    }
+
+
 }
